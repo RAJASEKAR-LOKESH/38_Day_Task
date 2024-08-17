893955b292936734a960ecc29fcf07f2491a61e7
RAJASEKAR L <84315997+RAJASEKAR-LOKESH@users.noreply.github.com>
2024-08-17 22:11:26 +0530

    Update connection.js

--- db/connection.js
+++ db/connection.js
@@ -1,6 +1,7 @@
 const mongoose=require('mongoose')
+const mongourl="mongodb://localhost:27017/Hall_Booking"
 const connection=async()=>{
-    await mongoose.connect("mongodb://localhost:27017/Hall_Booking")
+    await mongoose.connect(mongourl)
     console.log("We are connected to Mongodb")
 }
 // connection()
